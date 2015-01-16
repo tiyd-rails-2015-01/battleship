3eed0553de9b94b8eb21e368477ae5306d1b5505
Chris Foster <fosterchris22@gmail.com>
2015-01-16 11:50:27 -0500

    Test 3 and 4 pass

--- ship.rb
+++ ship.rb
@@ -2,10 +2,31 @@ class Ship
 
   def initialize(length)
     @length = length
+    @coordinates = []
   end
 
-def length
-  @length
-end
+  def length
+    @length
+  end
+
+  def place(column, row, across)
+    if across == true
+      @length.times do |i|
+        @coordinates << [column + i, row]
+      end
+    else
+      @length.times do |i|
+        @coordinates << [column, row + i]
+      end
+    end
+  end
+
+  def covers?(column, row)
+    if @coordinates.include? [column, row]
+      return true
+    else
+      return false
+    end
+  end
 
 end
