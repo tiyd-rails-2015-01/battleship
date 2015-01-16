f00e36428ada11989ae3c8718cf2f32da6885e8c
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-16 12:57:26 -0500

    Added code and finished test 3 4 and 5

--- battleship.rb
+++ battleship.rb
@@ -1,20 +1,43 @@
 class Ship
-  def initialize(ship)
-    @ship = ship
+  def initialize(length)
+    @length = length
+    @coordinates = []
+    @placed = false
   end
   def length
-    length = 4
+    return @length
   end
-  def place(place_l, place_r, good)
-    @place_l = place_l
-    @place_r = place_r
-    @good = good
+  def place(x_axis, y_axis, across)
+    if @placed == false
+      if across
+        @length.times do |i|
+          @coordinates << [x_axis + i, y_axis]
+        end
+      else # = vertical
+        @length.times do |i|
+          @coordinates << [x_axis, y_axis + i]
+        end
+      end
+      @placed = true
+    end
   end
-  def covers?(covers_l, covers_r)
-    @covers_l = [2, 3, 4, 5]
-    @covers_r = 1
+  def covers?(x_axis, y_axis)
+    @coordinates.include? [x_axis, y_axis]
   end
-  def overlaps_with
+  def overlaps_with?(ship)
+    overlap = false
+    @coordinates.each do |coordinates|
+        x_axis = coordinates[0]
+        y_axis = coordinates[1]
+      if ship.covers?(x_axis, y_axis)
+        overlap = true
+      end
+    end
+    return overlap
+  end
+  def fire_at(x_axis, y_axis)
+    hit = true
+    
 
   end
 
