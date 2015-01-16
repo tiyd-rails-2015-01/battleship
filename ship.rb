d89dd2877584c9bfd9b12068d37ea9ec2db36341
Chris Foster <fosterchris22@gmail.com>
2015-01-16 12:24:02 -0500

    Test 5 passes

--- ship.rb
+++ ship.rb
@@ -3,6 +3,7 @@ class Ship
   def initialize(length)
     @length = length
     @coordinates = []
+    @placed = false
   end
 
   def length
@@ -10,19 +11,31 @@ class Ship
   end
 
   def place(column, row, across)
-    if across == true
-      @length.times do |i|
-        @coordinates << [column + i, row]
-      end
-    else
-      @length.times do |i|
-        @coordinates << [column, row + i]
+    if @placed == false
+      @placed = true
+
+      if across == true
+        @length.times do |i|
+          @coordinates << [column + i, row]
+        end
+      else
+        @length.times do |i|
+          @coordinates << [column, row + i]
+        end
       end
     end
   end
 
   def covers?(column, row)
     if @coordinates.include? [column, row]
+      return true
+    else
+      return false
+    end
+  end
+
+  def overlaps_with?(ship)
+    if (@coordinates).empty?
       return true
     else
       return false
