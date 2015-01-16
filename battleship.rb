d07d65e97b482bea4035152ca47ec3c90c802234
Drew Smith <greenwichdrew@gmail.com>
2015-01-16 13:45:10 -0500

    second commit

--- battleship.rb
+++ battleship.rb
@@ -2,20 +2,46 @@ class Ship
 
   def initialize(length)
     @length = length
+    @coord = []
+    @placed = false
   end
 
   def length
-    @length
+    return @length
   end
 
-  def place(across, down, location)
-    @across = across
-    @down = down
-    @location = true
+  def place(x, y, horiz)
+    if @placed == false
+      if horiz
+        @length.times do |i|
+          @coord << [x + i, y]
+        end # do
+      else # if horiz
+        @length.times do |i|
+          @coord << [x, y + i]
+        end # do
+      end # if horiz
+    @placed = true
+    end # if @place == false
+  end # def place
+
+  def covers?(x, y)
+    @coord.include? [x, y]
+    @x = x
+    @y = y
   end
 
-  def covers
-  
+  def overlaps_with?(ship)
+    overlap = false
+    @coord.each do |coordinates|
+      @x = coordinates[0]
+      @y = coordinates[1]
+      if ship.covers?[x, y]
+        overlap = true
+      end
+    end
+
   end
+
 
 end
