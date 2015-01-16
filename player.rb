f657d2cc1c6bf0a8cc84d288d27d5abb539cabb3
C Haden <chaden@spoonflower.com>
2015-01-16 00:08:25 -0500

    Lots of tests passed. Stopping for the night before my eyes fall out

--- player.rb
+++ player.rb
@@ -1,15 +1,20 @@
+require './board.rb'
+
 class Player
 
   attr_reader :name
-  
+  attr_reader :board
+
   def initialize
     @name = "default"
+    @board = Board.new
   end
 end
 
 class HumanPlayer < Player
   def initialize( name = "Dave" )
     @name = name
+    @board = Board.new
   end
 end
 
@@ -17,5 +22,7 @@ class ComputerPlayer < Player
 
   def initialize
     @name = "HAL 9000"
+    @board = Board.new
+
   end
 end
