8979e55d7fbaffdbed79bfb7869290b8ee81788a
Drew Smith <greenwichdrew@gmail.com>
2015-01-15 23:34:17 -0500

    first commit

---
+++ battleship.rb
@@ -0,0 +1,21 @@
+class Ship
+
+  def initialize(length)
+    @length = length
+  end
+
+  def length
+    @length
+  end
+
+  def place(across, down, location)
+    @across = across
+    @down = down
+    @location = true
+  end
+
+  def covers
+  
+  end
+
+end
