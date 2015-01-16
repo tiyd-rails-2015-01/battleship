b347b50534cb7b51a520e2a65d8dbb72b7036185
Fallonious <FallonSkinner@gmail.com>
2015-01-16 16:34:25 -0500

    f_4th_error

--- ship.rb
+++ ship.rb
@@ -1,19 +1,31 @@
 class Ship
   def initialize(shiplength)
     @shiplength = shiplength
+    @locations = []
   end
 
   def length
     return @shiplength
       #use @instance variable to indicate that you want to apply this to all the examples
-  end  
+  end
+
+  def place(x, y, direction)
+    if direction
+      @locations = [[x,y],[x+1,y],[x+2,y],[x+3,y]]
+    else
+      @locations = [[x,y],[x,y+1],[x,y+2],[x,y+3]]
+    end
+  end
+
+  def covers?(x,y)
+    search_for = [x, y]
+    @locations.include?(search_for) 
+  end
+
 end
 
+#ship = Ship.new(4)
+#ship.place(2, 1, true)
 
 
-
-carrier = Ship.new(5)
-battleship = Ship.new( 4)
-cruiser = Ship.new(3)
-submarine = Ship.new(3)
-destroyer = Ship.new(2)
+#ship.place(2, 2, false)
