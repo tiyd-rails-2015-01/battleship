a3922d3f86dfb6e08457ff1498dc347d4e17edb9
Mike Marbry <mlmarbry@gmail.com>
2015-01-16 12:57:27 -0500

    Passed to Board

--- ship.rb
+++ ship.rb
@@ -4,6 +4,7 @@ class Ship
     @length = length
     @covered_coordinates = []
     @placed = false
+    @ship_hits = 0
   end
 
   def length
@@ -30,22 +31,33 @@ class Ship
     @covered_coordinates.include?([x, y])
   end
 
-  def overlap
-    if !(@covered_coordinates).empty?
-      false
-    else
-      true
+  def overlaps_with?(other_ship)
+    do_they_overlap = false
+    @covered_coordinates.each do |coord|
+      x = coord[0]
+      y = coord[1]
+      if other_ship.covers?(x, y)
+        do_they_overlap = true
+      end
     end
+      return do_they_overlap
   end
 
   def fire_at(x, y)
-    if @covered_coordinates.include?
-      ship.hits += 1
+    is_ship_hit = false
+    if @covered_coordinates.include? [x, y]
+        is_ship_hit = true
+        @ship_hits += 1
     end
+    return is_ship_hit
+
   end
 
-  def sunk
-    if ship.hits == ship.length
+  def sunk?
+    ship_sunk = false
+    if @ship_hits == @length
+      sunk = true
     end
+    return sunk
   end
 end
