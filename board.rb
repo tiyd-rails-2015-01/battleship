51d3416f031d921a1aa4daae4540f2ea0d027c21
Topstitch <kallen@spoonflower.com>
2015-01-16 15:52:26 -0500

    asking for test 11 now

--- board.rb
+++ board.rb
@@ -1,6 +1,33 @@
 class Board
-  @has_ships_there = false
-  def has_ship_on?(x, y)
-    false
+  def initialize()
+    @has_ships = []
   end
+  def has_ship_on?(x, y)
+    covered = false
+    @has_ships.each do |ship|
+      if ship.covers?(x, y)
+        covered = true
+      end
+    end
+    return covered
+  end
+  def place_ship(ship, x, y, across)
+      ship.place(x, y, across)
+      @has_ships << ship
+  end
+
 end
+
+# @ships = all_ships
+# covered = false
+# ships.each do |ship|
+# if ship.covers(x, y)
+#   covered = true
+#   break
+# end
+# end
+# if !covered
+#   actually place
+#
+# else false
+# end
