620a88435bab7c8d285b07c9bea01f29b80cd433
Fallonious <FallonSkinner@gmail.com>
2015-01-16 17:41:30 -0500

    f_been_placed

--- ship.rb
+++ ship.rb
@@ -2,6 +2,7 @@ class Ship
   def initialize(shiplength)
     @shiplength = shiplength
     @locations = []
+    @has_been_placed = false
   end
 
   def length
@@ -10,18 +11,27 @@ class Ship
   end
 
   def place(x, y, direction)
-    if direction
-      @locations = [[x,y],[x+1,y],[x+2,y],[x+3,y]]
-    else
-      @locations = [[x,y],[x,y+1],[x,y+2],[x,y+3]]
+
+    if @has_been_placed == false # ______  == _______ # has already been placed
+      if direction
+        @locations = [[x,y],[x+1,y],[x+2,y],[x+3,y]]
+      else
+        @locations = [[x,y],[x,y+1],[x,y+2],[x,y+3]]
+      end
+      @has_been_placed = true
     end
+
+
+
+    #  [x,y] == [x,y]
+
   end
+
 
   def covers?(x,y)
     search_for = [x, y]
-    @locations.include?(search_for) 
+    @locations.include?(search_for)
   end
-
 end
 
 #ship = Ship.new(4)
