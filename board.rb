d5537236d3cd40319079fff558780912816b454a
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-16 22:44:55 -0500

    passed test 14

--- board.rb
+++ board.rb
@@ -31,31 +31,19 @@ class Board
     end
   end
   def fire_at(x_axis, y_axis)
-    hit = false
+    hit = true
     attack = [x_axis, y_axis]
     @fleet.each do |ship|
-      # x_axis = ship[0]
-      # y_axis = ship[1]
-      if @shots_fired.include?(attack)
-        hit = true
-        # @shots_fired << attack
+      if ship.covers?(x_axis, y_axis)
+        hit = false
       end
     end
     if !hit
-      @shots_fired << attack
+    @shots_fired << attack
       return true
     else
+    @shots_fired << attack
       return false
-
     end
   end
 end
-
-
-# ship.place(x_axis, y_axis, across)
-# hit = false
-# @fleet.each do |ship|
-#   if ship.place_ship?(x_axis, y_axis)
-#     hit = true
-#   end
-# end
