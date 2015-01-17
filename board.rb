3e088c743a8d4da7b702c819935297459995b857
Drew Smith <greenwichdrew@gmail.com>
2015-01-17 14:53:18 -0500

    board test 10

---
+++ board.rb
@@ -0,0 +1,23 @@
+require './ship'
+
+class Board
+  def initialize
+    @fleet = []
+    @placed = []
+  end
+
+  def has_ship_on?(x, y)
+    ship_on = false
+    @fleet.each do |ship|
+      if ship.covers?(x, y)
+        ship_on = true
+      end
+    end
+    return ship_on
+  end
+  def place_ship(ship, x, y, direction)
+    ship.place(x, y, direction)
+    @fleet << ship
+    return true
+    end
+  end
--- ship.rb
+++ ship.rb
@@ -1,2 +1,56 @@
-class ship
+class Ship
+
+  def initialize(length)
+    @length = length
+    @hit_points = @length
+    @coord = []
+    @placed = false
+  end
+
+  def length
+    return @length
+  end
+
+  def place(x, y, horiz)
+    if @placed == false
+      if horiz
+        @length.times do |i|
+          @coord << [x + i, y]
+        end # do
+      else # if horiz
+        @length.times do |i|
+          @coord << [x, y + i]
+        end # do
+      end # if horiz
+      @placed = true
+    end
+  end
+
+  def covers?(x, y)
+    @coord.include? [x, y]
+  end
+
+  def overlaps_with?(ship)
+    overlap = false
+    @coord.each do |co|
+      if ship.covers?(co[0], co[1])
+        overlap = true
+      end
+    end
+    return overlap
+  end
+
+  def fire_at(x, y)
+    if @coord.include?([x, y])
+      @hit_points -= 1
+      return true
+    end
+  end
+
+  def sunk?
+    if @hit_points == 0
+      return true
+    end
+  end
+
 end
