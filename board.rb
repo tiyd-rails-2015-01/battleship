eec95c8b77472155db9291c401843f0be4931aa9
Fallonious <FallonSkinner@gmail.com>
2015-01-17 14:18:58 -0500

    f_9th

--- board.rb
+++ board.rb
@@ -1,4 +1,17 @@
+#require './ship.rb'
+
 class Board
-  def initialize()
+  def initialize
+    @fleet = []
+  end
+
+  def has_ship_on?(x, y)
+    ship_on = false
+    @fleet.each do |ship|
+      if ship.covers?(x, y)
+        ship_on = true
+      end
+    end
+    return ship_on
   end
 end
