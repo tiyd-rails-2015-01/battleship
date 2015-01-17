1eddd0001e4c73ab7975b4f6622ff9056a7bd7b3
Fallonious <FallonSkinner@gmail.com>
2015-01-17 11:44:22 -0500

    f_6th_error

--- ship.rb
+++ ship.rb
@@ -3,6 +3,8 @@ class Ship
     @shiplength = shiplength
     @locations = []
     @has_been_placed = false
+    @overlaps_with = false
+
   end
 
   def length
@@ -11,7 +13,6 @@ class Ship
   end
 
   def place(x, y, direction)
-
     if @has_been_placed == false # ______  == _______ # has already been placed
       if direction
         @locations = [[x,y],[x+1,y],[x+2,y],[x+3,y]]
@@ -20,17 +21,27 @@ class Ship
       end
       @has_been_placed = true
     end
-
-
-
-    #  [x,y] == [x,y]
-
   end
-
 
   def covers?(x,y)
     search_for = [x, y]
     @locations.include?(search_for)
+  end
+
+  def locations
+    @locations
+  end
+
+  def overlaps_with?(othership)
+#    @locations.each do |check|
+#      @locations & othership.locations[x,y]
+#    end
+      if
+      (@locations & othership.locations).empty?
+       return false
+      else
+        return true
+      end
   end
 end
 
