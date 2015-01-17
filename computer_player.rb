4d9f7940615d2025ed09e4ff9cd501766b8d2383
Jesseract <jlpearce@gmail.com>
2015-01-17 17:49:38 -0500

    Test26

--- computer_player.rb
+++ computer_player.rb
@@ -1,10 +1,17 @@
+require "./board.rb"
+
 class ComputerPlayer < Player
+  attr_accessor :board
+
   def initialize
+    @board = Board.new
 
   end
 
   def name
     name = "HAL 9000"
   end
+
+
 
 end
--- human_player.rb
+++ human_player.rb
@@ -1,9 +1,20 @@
 require "./player.rb"
+require "./board.rb"
 
 class HumanPlayer < Player
-  attr_accessor :name
+  attr_accessor :name, :board
+
   def initialize(name = "Dave")
     @name = name
+    @board = Board.new
   end
+
+  # def board
+  #   @board
+  # end
+
+
+
+
 
 end
