d2b6fdea809855cfbc66c7ffb04f8314e74fa061
Topstitch <kallen@spoonflower.com>
2015-01-17 19:55:19 -0500

    AAs down from first ship

--- board.rb
+++ board.rb
@@ -35,28 +35,32 @@ class Board
        end
     end
   end
-  def fleet_coords
-    return @fleet
+  def fleet_positions
+    coordinates = []
+    @fleet.each do |ship|
+      ship.show_coords.each do |c|
+        coordinates << c
+      end
+    end
+    return coordinates
   end
   def display
     counter = (1..10).to_a
     letters = ("A |".."J |").to_a
-    boxes = []
+    ship_coords = fleet_positions
+    #   container_of_rows.each do |pair|
+      #end
+    boxes = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
     puts"    1   2   3   4   5   6   7   8   9   10"
     puts"  -----------------------------------------"
     counter.each do |square|
-      now_letter = letters[square-1]
-      print "#{now_letter}"
-      counter.each do |look|
-        box = boxes[look-1]
-        if
-          boxes << "O"
-        else
-          boxes << " "
+      print "#{letters[square-1]}"
+      ship_coords.each do |coords|
+        if coords[1] == counter[square-1]
+          boxes[coords[0]-1] = "A"
         end
       end
-      print " #{boxes[0]} | #{boxes[1]} | #{boxes[2]} | #{boxes[3]} | #{boxes[4]} | #{boxes[5]} | #{boxes[6]} | #{boxes[7]} | #{boxes[8]} | #{boxes[9]} |"
-      print "\n"
+      puts " #{boxes[0]} | #{boxes[1]} | #{boxes[2]} | #{boxes[3]} | #{boxes[4]} | #{boxes[5]} | #{boxes[6]} | #{boxes[7]} | #{boxes[8]} | #{boxes[9]} |"
     end
     puts"  -----------------------------------------"
   end
