dd7dcf92764b26cfbfc155798c2439a8877f50b5
Fallonious <FallonSkinner@gmail.com>
2015-01-17 19:19:11 -0500

    f_17th

--- board.rb
+++ board.rb
@@ -4,6 +4,7 @@ class Board
   def initialize
     @fleet = []
     @placed = []
+    @shotstaken = []
 
 
   end
@@ -31,12 +32,50 @@ class Board
 
 
   def fire_at(x, y)
-    @fleet.each do |ship|
-      if ship.fire_at(x,y)
-        return true
+    if @shotstaken.include?([x,y])
+      return false  #shot already taken
+    else
+      @shotstaken << [x,y]
+      @fleet.each do |ship|
+        if ship.fire_at(x,y)
+          return true #shot taken & hit
+        end
       end
     end
-    return false
+    return false #representing a miss
   end
+
+  def display
+    puts   %Q{    1   2   3   4   5   6   7   8   9   10
+  -----------------------------------------
+A |   |   |   |   |   |   |   |   |   |   |
+B |   |   |   |   |   |   |   |   |   |   |
+C |   |   |   |   |   |   |   |   |   |   |
+D |   |   |   |   |   |   |   |   |   |   |
+E |   |   |   |   |   |   |   |   |   |   |
+F |   |   |   |   |   |   |   |   |   |   |
+G |   |   |   |   |   |   |   |   |   |   |
+H |   |   |   |   |   |   |   |   |   |   |
+I |   |   |   |   |   |   |   |   |   |   |
+J |   |   |   |   |   |   |   |   |   |   |
+  -----------------------------------------
+}
+  end
+
+
+
+  #["A", "B", "C", "D", "E", "F", "G", ""]
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
