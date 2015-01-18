07a30b65b9931305c69d198fa0d2eec9a60c28a1
Owen <owenrham@gmail.com>
2015-01-17 21:03:41 -0500

    Done for the night.

--- human_player.rb
+++ human_player.rb
@@ -2,10 +2,24 @@ require './player'
 require './board'
 require './ship'
 
+def get_user_input
+  gets.chomp
+end
+
 class HumanPlayer < Player
   attr_accessor :name, :board
   def initialize(name = "Dave")
     @name = name
     @board = Board.new
+  end
+
+  def ships
+    
+  end
+
+  def place_ships(lengths)
+    print "#{@name}, where would you like to place a ship of length 2?\nAcross or Down?\n"+
+          "#{@name}, where would you like to place a ship of length 5?\nAcross or Down?\n"
+    true
   end
 end
