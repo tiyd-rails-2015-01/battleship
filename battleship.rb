ed017a36b60bd530ac355297926cb7d2792370a8
Rocky Randalls <rrandalls@gmail.com>
2015-01-19 15:18:46 -0500

    Modified human player class, now working on test 31

--- battleship.rb
+++ battleship.rb
@@ -3,3 +3,13 @@ require './classes/board.rb'
 require './classes/player.rb'
 require './classes/humanplayer.rb'
 require './classes/computerplayer.rb'
+
+
+# Debug asking human player for ships (test 30)
+# player = HumanPlayer.new
+# player.place_ships([2, 5])
+# puts "ship length: "
+# puts player.ships.length
+# puts player.ships[1].length
+# puts player.board.has_ship_on?(1, 1)
+# puts player.board.has_ship_on?(4, 1)
--- classes/humanplayer.rb
+++ classes/humanplayer.rb
@@ -1,7 +1,31 @@
+def get_user_input
+  gets.chomp
+end
+
 class HumanPlayer < Player
   attr_accessor :name
+
   def initialize(input = "Dave")
     @board = Board.new
     @name = input
   end
+
+  def ships
+    board.ships
+  end
+
+  def place_ships(ship_lengths)
+    ship_lengths.each do |length|
+      puts "#{@name}, where would you like to place a ship of length #{length}?"
+      coordinate = get_user_input
+      x = @board.x_of(coordinate)
+      y = @board.y_of(coordinate)
+      ship = Ship.new(length)
+      puts "Across or Down?"
+      across = get_user_input
+      is_it_across = across.include?("across")
+      board.place_ship(ship, x, y, is_it_across)
+    end
+  end
+
 end
