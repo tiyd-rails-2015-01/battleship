9e8509df0549d3503686592148b17465668631ea
Jesseract <jlpearce@gmail.com>
2015-01-19 16:22:17 -0500

    test 33

--- game.rb
+++ game.rb
@@ -1,6 +1,24 @@
 require "./player.rb"
 
+def get_user_input
+  gets.chomp
+end
+
 class Game
-  def initialize
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+
   end
+
+
+  def welcome
+    puts "Welcome, #{@player_1.name} and #{@player_2.name}!\nIt's time to play Battleship.\n"
+
+  end
+
+  def place_ships
+  end
+
+
 end
--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 class Player
   attr_reader :board
+  attr_reader :name
   def initialize
     @board = Board.new
   end
