aa4685d298ee6c64876f35ef858a5faa609fd66f
Topstitch <kallen@spoonflower.com>
2015-01-19 16:41:56 -0500

    test 30 done

--- human_player.rb
+++ human_player.rb
@@ -1,3 +1,7 @@
+def get_user_input
+  gets.chomp
+end
+
 require './ship.rb'
 require './board.rb'
 
@@ -11,5 +15,26 @@ class HumanPlayer < Player
   end
   def board
     return @board
+  end
+  def place_ships(array_of_ship_lengths)
+    array_of_ship_lengths.each do |length|
+      ship = Ship.new(length)
+      puts "#{@name}, where would you like to place a ship of length #{length}?"
+      coord = get_user_input
+      x = @board.x_of(coord)
+      y = @board.y_of(coord)
+      puts "Across or Down?"
+      direction = get_user_input
+      a = true
+      if direction == "Across"
+        a = true
+      else
+        a = false
+      end
+      @board.place_ship(ship, x, y, a)
+    end
+  end
+  def ships
+    return @board.fleet
   end
 end
--- try.rb
+++ try.rb
@@ -1,38 +1,43 @@
 require './ship.rb'
 require './board.rb'
 require './player.rb'
-#require './computer_player.rb'
+require './computer_player.rb'
 require './human_player.rb'
-class ComputerPlayer < Player
-  def initialize
-    @board = Board.new
-  end
-  def name
-    return "HAL 9000"
-  end
-  def board
-    return @board
-  end
-  def place_ships(lengths)
-    lengths.each do |l|
-      ship_placed = false
-      until ship_placed == true do
-        ship_placed = place_ship(l)
-      end
-    end
-    puts "HAL 9000 has placed his ships"
-    return true
-  end
-  def ships
-    return lengths
-  end
-end
+
+player = HumanPlayer.new
+player.place_ships([2,5])
 
 
-
-player = ComputerPlayer.new
-
-player.place_ships([2, 3, 3, 4, 5])
-
-puts "#{player.ships.length}"
-puts "#{player.ships[3].length}"
+# class ComputerPlayer < Player
+#   def initialize
+#     @board = Board.new
+#   end
+#   def name
+#     return "HAL 9000"
+#   end
+#   def board
+#     return @board
+#   end
+#   def place_ships(lengths)
+#     lengths.each do |l|
+#       ship_placed = false
+#       until ship_placed == true do
+#         ship_placed = place_ship(l)
+#       end
+#     end
+#     puts "HAL 9000 has placed his ships"
+#     return true
+#   end
+#   def ships
+#     return lengths
+#   end
+# end
+#
+#
+#
+# player = ComputerPlayer.new
+#
+# player.place_ships([2, 3, 3, 4, 5])
+#
+# puts "#{player.ships.length}"
+# puts "#{player.ships[3].length}"
