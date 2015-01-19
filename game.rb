8b01ac00bd80b9e68e69a7f7d3040924aad2362d
Topstitch <kallen@spoonflower.com>
2015-01-19 17:38:03 -0500

    passed welcome

--- game.rb
+++ game.rb
@@ -1,6 +1,11 @@
+
+
 class Game
   def initialize(human, computer)
-    @human = HumanPlayer.new
-    @computer = ComputerPlayer.new
+    @human = human
+    @computer = computer
+  end
+  def welcome
+    puts "Welcome, #{@human.name} and #{@computer.name}!\nIt's time to play Battleship."
   end
 end
