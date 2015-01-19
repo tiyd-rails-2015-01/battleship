3bbb01e32f982bd26344ed30785c58020619515d
Jesseract <jlpearce@gmail.com>
2015-01-19 18:05:47 -0500

    test 37, 38

--- game.rb
+++ game.rb
@@ -7,24 +7,54 @@ def get_user_input
 end
 
 class Game
-  def initialize(human, computer, ships = [2, 3, 3, 4, 5])
-    @human = human
-    @computer = computer
+  def initialize(player_1, player_2, ships = [2, 3, 3, 4, 5])
+    @player_1 = player_1
+    @player_2 = player_2
     @ships = ships
+    @player_1_goes = true
 
   end
 
 
   def welcome
-    puts "Welcome, #{@human.name} and #{@computer.name}!\nIt's time to play Battleship.\n"
+    puts "Welcome, #{@player_1.name} and #{@player_2.name}!\nIt's time to play Battleship.\n"
 
   end
 
   def place_ships
-    @human.place_ships(@ships)
-    @computer.place_ships(@ships)
-
+    @player_1.place_ships(@ships)
+    @player_2.place_ships(@ships)
   end
+
+  def take_turn
+    # if @player_1_goes || player_2.class != ComputerPlayer
+    #   coord = get_user_input
+    # else
+    #   coord = "A1"
+    # end
+    #
+    # if player_1_goes
+    #   @player_1_goes = false
+    #   x = @player_2.board.x_of(coord)
+    #   y = player_2.board.y_of(coord)
+    #   if @player_2.board.fire_at(x, y)
+    #     puts "Hit!"
+    #   # @player_1.target_board.mark_target_board(x, y, "+")
+    #   # if player_2.board.sunk?
+    #   #   @game_over = true
+    #   #   puts "Congratulations, #{player_1.name}!"
+    #   # end
+    #   return
+    # else
+      puts "Miss!"
+      # @player_1.target_board.mark_target_board(x, y, "+")
+      # return
+  #   end
+  # else
+  #   player_1_goes = true
+  #   x = @
+  end
+
 
 
 end
