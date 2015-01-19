26c907d625a93184ca2a0a62228d7141c7eb457d
Jesseract <jlpearce@gmail.com>
2015-01-19 16:57:36 -0500

    test 35

--- game.rb
+++ game.rb
@@ -1,23 +1,29 @@
 require "./player.rb"
+require "./human_player.rb"
+require "./computer_player.rb"
 
 def get_user_input
   gets.chomp
 end
 
 class Game
-  def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+  def initialize(human, computer, ships = [2, 3, 3, 4, 5])
+    @human = human
+    @computer = computer
+    @ships = ships
 
   end
 
 
   def welcome
-    puts "Welcome, #{@player_1.name} and #{@player_2.name}!\nIt's time to play Battleship.\n"
+    puts "Welcome, #{@human.name} and #{@computer.name}!\nIt's time to play Battleship.\n"
 
   end
 
   def place_ships
+    @human.place_ships(@ships)
+    @computer.place_ships(@ships)
+
   end
 
 
