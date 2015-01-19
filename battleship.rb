4fad65e76a0c71bd5664904f8b3bead4c1dba2fa
Jesseract <jlpearce@gmail.com>
2015-01-19 17:29:41 -0500

    test 36

--- battleship.rb
+++ battleship.rb
@@ -4,3 +4,10 @@ require './player.rb'
 require './human_player.rb'
 require './computer_player.rb'
 require './game.rb'
+
+# human = HumanPlayer.new("Alice")
+# computer = ComputerPlayer.new
+# game = Game.new(human, computer, [2, 3])
+#   game.place_ships
+#
+# human.board.display
--- human_player.rb
+++ human_player.rb
@@ -20,15 +20,27 @@ class HumanPlayer < Player
 
   def place_ships(ship_lengths)
     ship_lengths.each do |l|
+      placed = false
       puts "#{name}, where would you like to place a ship of length #{l}?"
-      coordinate = get_user_input
-      x = @board.x_of(coordinate)
-      y = @board.y_of(coordinate)
-      ship = Ship.new(l)
-      puts "Across or Down?"
-      across = get_user_input
-      is_it_across = across.include?("across")
-      board.place_ship(ship, x, y, is_it_across)
+
+      until placed
+        coordinate = get_user_input
+        x = @board.x_of(coordinate)
+        y = @board.y_of(coordinate)
+        puts "Across or Down?"
+
+
+        across = get_user_input
+        is_it_across = across.downcase.include?("across")
+
+        ship = Ship.new(l)
+        placed = board.place_ship(ship, x, y, is_it_across)
+
+        unless placed
+        puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
+        puts "#{name}, where would you like to place a ship of length #{l}?"
+        end
+      end
     end
   end
 
