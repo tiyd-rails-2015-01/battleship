599c4c110d1013f663a58faeec4ffd52231efcf3
Topstitch <kallen@spoonflower.com>
2015-01-19 17:50:22 -0500

    done with 35

--- game.rb
+++ game.rb
@@ -1,11 +1,16 @@
 
 
 class Game
-  def initialize(human, computer)
+  def initialize(human, computer, ships=[2, 3, 3, 4, 5])
     @human = human
     @computer = computer
+    @ships = ships
   end
   def welcome
     puts "Welcome, #{@human.name} and #{@computer.name}!\nIt's time to play Battleship."
+  end
+  def place_ships
+    @human.place_ships(@ships)
+    @computer.place_ships(@ships)
   end
 end
