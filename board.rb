1145c41cd08b271e492319b1dffaf41e745d7e84
Ruben <ssrcruz@gmail.com>
2015-01-19 13:39:25 -0500

    On Test 10.

--- board.rb
+++ board.rb
@@ -9,11 +9,34 @@ class Board
 
   def has_ship_on?(x, y)
 
-    @ships.each do |ships|
-      if ships.covers?([x, y])
-        return ships
-      end
-    end
-  end
+    if @ships.each do |ship|
+      if ship.covers?([x, y])
+        @ships << ship
+        return true
+      else
 
-end
+        return false
+
+      end # if ship.covers?
+
+      end # else end
+    end # if @ships.each
+  end # def
+
+  def place_ship(ship, x, y, horiz)
+    ship.place(x, y, horiz)
+    @ships.each do |ship|
+      if ship.overlaps_with?(ship)
+        return false
+        end # if end
+      end # if end
+      @ships << ship
+    end # do end
+  end # def end
+end # class end
+
+
+
+
+  end
+end # class
