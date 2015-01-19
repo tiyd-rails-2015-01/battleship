e99899c0eaa4aa79798ad653c04977a571bce032
Topstitch <kallen@spoonflower.com>
2015-01-18 21:31:04 -0500

    finally test 19 done

--- board.rb
+++ board.rb
@@ -44,23 +44,28 @@ class Board
     end
     return coordinates
   end
+  def locations_of_hits
+    return @has_been_shot
+  end
   def display
     puts"    1   2   3   4   5   6   7   8   9   10"
     puts"  -----------------------------------------"
     row = (1..10).to_a
     ship_coords = fleet_positions
+    red_xes = locations_of_hits
     letters = ("A |".."J |").to_a
     row.each do |num|
-
       boxes = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
-
       ship_coords.each do |coord|
         if coord[1] == num
           boxes[coord[0]-1] = "O"
         end
-
       end
-
+      red_xes.each do |c|
+        if c[1] == num
+          boxes[c[0]-1] = "X"
+        end
+      end
       puts "#{letters[num-1]} #{boxes[0]} | #{boxes[1]} | #{boxes[2]} | #{boxes[3]} |" +
       " #{boxes[4]} | #{boxes[5]} | #{boxes[6]} | #{boxes[7]} | #{boxes[8]} | #{boxes[9]} |"
     end
