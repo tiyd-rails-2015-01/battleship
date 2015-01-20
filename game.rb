04d9f14606585acff294c6eaceb0a53c45fc75e9
Topstitch <kallen@spoonflower.com>
2015-01-20 07:54:57 -0500

    still working on 40, but without breaking anything else

--- game.rb
+++ game.rb
@@ -3,29 +3,33 @@ def get_user_input
 end
 
 class Game
-  def initialize(human, computer, ships=[2, 3, 3, 4, 5])
-    @human = human
-    @computer = computer
+  def initialize(player1, player2=ComputerPlayer.new, ships=[2, 3, 3, 4, 5])
+    @player1 = player1
+    @player2 = player2
     @ships = ships
     @turn_counter = 0
     @won = false
   end
   def welcome
-    puts "Welcome, #{@human.name} and #{@computer.name}!\nIt's time to play Battleship."
+    puts "Welcome, #{@player1.name} and #{@player2.name}!\nIt's time to play Battleship."
   end
   def place_ships
-    @human.place_ships(@ships)
-    @computer.place_ships(@ships)
+    @player1.place_ships(@ships)
+    @player2.place_ships(@ships)
   end
   def take_turn
+
     if @turn_counter.even?
       @turn_counter += 1
-      puts "Take a shot!"
-      coords = get_user_input
-
-      x = @human.board.x_of(coords)
-      y = @human.board.y_of(coords)
-      shot = @human.board.fire_at(x, y)
+      if @player1.class == HumanPlayer
+        puts "Take a shot!"
+        @coords = get_user_input
+      else
+        @coords = "A1"
+      end
+      x = @player1.board.x_of(@coords)
+      y = @player1.board.y_of(@coords)
+      shot = @player1.board.fire_at(x, y)
       if shot == true
         puts "Hit!"
       else
@@ -33,9 +37,15 @@ class Game
       end
     else
       @turn_counter += 1
-      x = 1#rand(10)
-      y = 1#rand(10)
-      computer_shot = @computer.board.fire_at(x, y)
+      if @player2.class == HumanPlayer
+        puts "Take a shot!"
+        @coords = get_user_input
+      else
+        @coords = "A1"
+      end
+      x = @player2.board.x_of(@coords)
+      y = @player2.board.y_of(@coords)
+      shot = @player2.board.fire_at(x, y)
       if shot == true
         puts "Hit!"
       else
