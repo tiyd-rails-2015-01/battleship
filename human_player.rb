b0bb255cfbb4e90babaedfb15e7bf3e2de821fc0
Owen <owenrham@gmail.com>
2015-01-19 23:13:07 -0500

    It's late. I'm done

--- human_player.rb
+++ human_player.rb
@@ -1,5 +1,5 @@
 # require './player'
-# require './board'
+require './board'
 # require './ship'
 
 def get_user_input
@@ -11,9 +11,18 @@ class HumanPlayer < Player
   def initialize(name = "Dave")
     @name = name
     @board = Board.new
+    @ships = []
   end
 
   # def ships
   #
   # end
+
+  def place_ships(lengths)
+    print "#{@name}, where would you like to place a ship of length 2?\nAcross or Down?\n"+
+          "#{@name}, where would you like to place a ship of length 5?\nAcross or Down?\n"
+    lengths.each do |l|
+      @ships << Ship.new(l)
+    end
+  end
 end
--- player.rb
+++ player.rb
@@ -1,12 +1,9 @@
+require "./board"
+
 class Player
-  attr_accessor :board, :ships, :name
+  attr_accessor :board, :name, :ships
   def initialize
     @board = Board.new
-  end
-
-  def place_ships(lengths)
-    print "#{@name}, where would you like to place a ship of length 2?\nAcross or Down?\n"+
-    "#{@name}, where would you like to place a ship of length 5?\nAcross or Down?\n"
-    true
+    @ships = @fleet
   end
 end
