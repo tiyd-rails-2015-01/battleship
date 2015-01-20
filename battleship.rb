fbfe7d5da2ff24c12ab37117a5a0cd8b16fa656d
Jesseract <jlpearce@gmail.com>
2015-01-19 23:57:47 -0500

    one last thing

--- battleship.rb
+++ battleship.rb
@@ -5,6 +5,7 @@ require './human_player.rb'
 require './computer_player.rb'
 require './game.rb'
 
-
-game= Game.new(HumanPlayer.new, HumanPlayer.new("Bob"), [2, 3, 3, 4, 5])
-game.play
+if __FILE__ == $0
+  game= Game.new(HumanPlayer.new, HumanPlayer.new("Bob"), [2, 3, 3, 4, 5])
+  game.play
+end
